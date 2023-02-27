687a3863ec9446ce8240ac8b908bd3b750f277c5
Nabina aryal <106301164+nabinaaryal-2002@users.noreply.github.com>
2023-02-27 15:40:49 +0545

    changes

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:fluttersample1/models/bank.dart';
+import 'package:flutter/material.dart';
+
+
+
+void main(){
+  
+  final car = Car(Colors.black);
+  Bank bank1 = Bank(employerNumbers: 900,accountHolder: 'ram',bankAddress: 'kathmandu',bankName: 'NIBL');
+
+
+}
